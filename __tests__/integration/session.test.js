e2c9d92eb1a8c25d9b0359ddc584e1bd6f2494b4
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2019-03-07 08:59:30 -0300

    Middleware of authentication and coverage config

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,8 +1,8 @@
 const request = require('supertest');
 
 const app = require('../../src/app');
-const { User } = require('../../src/app/models');
 const truncate = require('../utils/truncate');
+const factory = require('../factories');
 
 describe('Authentic.ation', () => {
     beforeEach(async () => {
@@ -10,7 +10,10 @@ describe('Authentic.ation', () => {
     });
 
     it('Should authenticate with valid credentials', async () => {
-        const user = await User.create({ name: 'Gabriel', email: 'gabriel_hahn@hotmail.com', password: '123123' });
+        const user = await factory.create('User', {
+            password: '123123'
+        });
+
         const response = await request(app).post('/sessions').send({
             email: user.email,
             password: '123123'
@@ -20,7 +23,10 @@ describe('Authentic.ation', () => {
     });
 
     it('Should not authenticate with invalid credentials', async () => {
-        const user = await User.create({ name: 'Gabriel', email: 'gabriel_hahn@hotmail.com', password: '123123' });
+        const user = await factory.create('User', {
+            password: '123123'
+        });
+
         const response = await request(app).post('/sessions').send({
             email: user.email,
             password: '123456'
@@ -29,13 +35,37 @@ describe('Authentic.ation', () => {
         expect(response.status).toBe(401);
     });
 
-    it ('Should return JWT token when authenticated', async () => {
-        const user = await User.create({ name: 'Gabriel', email: 'gabriel_hahn@hotmail.com', password: '123123' });
+    it('Should return JWT token when authenticated', async () => {
+        const user = await factory.create('User', {
+            password: '123123'
+        });
+
         const response = await request(app).post('/sessions').send({
             email: user.email,
             password: '123123'
         });
 
         expect(response.body).toHaveProperty('token');
+    });
+
+    it('Should be able to access private routes when authenticated', async () => {
+        const user = await factory.create('User');
+
+        const response = await request(app).get('/dashboard')
+            .set('Authorization', `Bearer ${user.generateToken()}`);
+
+        expect(response.status).toBe(200);
+    });
+
+    it ('Should not be able to access private routes without JWT token', async () => {
+        const response = await request(app).get('/dashboard');
+        expect(response.status).toBe(401);
+    });
+
+    it ('Should not be able to access private routes with invalid JWT token', async () => {
+        const response = await request(app).get('/dashboard')
+            .set('Authorization', `Bearer 123123`);
+
+        expect(response.status).toBe(401);
     });
 });
